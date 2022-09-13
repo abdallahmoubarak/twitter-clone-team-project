2bb57fd34029617b476a9f62a7f6389888835f0f
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-13 20:02:33 +0300

    functioning close and open in sign up form

--- frontend/js/sign.js
+++ frontend/js/sign.js
@@ -1,1 +1,14 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  //  openning and clossing the sign popup
+  const signUpPopBtn = document.getElementById("sign-up-pop-btn");
+
+  signUpPopBtn.addEventListener("click", () => {
+    document.getElementById("signUp-popUp").style.display = "flex";
+
+    const closeBtn = document.getElementById("close-signUp");
+
+    closeBtn.addEventListener("click", () => {
+      document.getElementById("signUp-popUp").style.display = "none";
+    });
+  });
+});
