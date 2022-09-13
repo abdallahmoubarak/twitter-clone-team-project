cef8eaa2348dc20b6a37ebeb6ff037a91217718e
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-13 21:56:45 +0300

    login html

--- frontend/js/sign.js
+++ frontend/js/sign.js
@@ -1,14 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
   //  openning and clossing the sign popup
   const signUpPopBtn = document.getElementById("sign-up-pop-btn");
+  const signUppopUp = document.getElementById("sign-up-pop-up");
 
   signUpPopBtn.addEventListener("click", () => {
-    document.getElementById("signUp-popUp").style.display = "flex";
+    signUppopUp.style.display = "flex";
 
-    const closeBtn = document.getElementById("close-signUp");
+    const closeBtn = document.getElementById("close-sign-up");
 
     closeBtn.addEventListener("click", () => {
-      document.getElementById("signUp-popUp").style.display = "none";
+      signUppopUp.style.display = "none";
     });
   });
 });
