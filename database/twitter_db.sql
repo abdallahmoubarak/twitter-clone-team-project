9f61542190564a429a8d6d4e4b02dd1d55d0ef76
Peter Saba <petersaba001@gmail.com>
2022-09-14 15:52:41 +0300

    tables creation sql

---
+++ database/twitter_db.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2022 at 02:49 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `twitter_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blocks`
+--
+-- Error reading structure for table twitter_db.blocks: #1932 - Table &#039;twitter_db.blocks&#039; doesn&#039;t exist in engine
+-- Error reading data for table twitter_db.blocks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `twitter_db`.`blocks`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follows`
+--
+-- Error reading structure for table twitter_db.follows: #1932 - Table &#039;twitter_db.follows&#039; doesn&#039;t exist in engine
+-- Error reading data for table twitter_db.follows: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `twitter_db`.`follows`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+-- Error reading structure for table twitter_db.likes: #1932 - Table &#039;twitter_db.likes&#039; doesn&#039;t exist in engine
+-- Error reading data for table twitter_db.likes: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `twitter_db`.`likes`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tweets`
+--
+-- Error reading structure for table twitter_db.tweets: #1932 - Table &#039;twitter_db.tweets&#039; doesn&#039;t exist in engine
+-- Error reading data for table twitter_db.tweets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `twitter_db`.`tweets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table twitter_db.users: #1932 - Table &#039;twitter_db.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table twitter_db.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `twitter_db`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
