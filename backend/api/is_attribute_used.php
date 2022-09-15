63f88b9b84b7dfc046ab68c592c67f441951a86d
Peter Saba <petersaba001@gmail.com>
2022-09-15 16:50:25 +0300

    finished function to check for availability of attribute

---
+++ backend/api/is_attribute_used.php
@@ -0,0 +1,22 @@
+<?php
+
+    // checks if a value is already assigned to the given attribute name in the users table
+    function isAttributeAlreadyUsed($attribute_name, $attribute_value){
+        include_once("connection.php");
+        $query = $mysqli->prepare("SELECT " . $attribute_name . " FROM users WHERE " . $attribute_name . "=?");
+        $query->bind_param('s', $attribute_value);
+        $query->execute();
+        $arr = $query->get_result();
+
+        $result = [];
+        while($value = $arr->fetch_assoc()){
+            $result[] = $value;
+        }
+        // if the result is empty then the given value does not exist in the table
+        if(!$result){
+            return FALSE;
+        }else{
+            return TRUE;
+        }
+    }
+?>
