7063b756cf79651b29ec16780b5fbd29edbd9db7
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-15 17:20:33 +0300

    fixing bug

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -4,4 +4,23 @@ window.addEventListener("DOMContentLoaded", () => {
   homeBtn.addEventListener("click", () => {
     window.location.href = "/frontend/home.html";
   });
+
+  //   log out
+
+  const popBackground = document.getElementById("pop-background");
+  const userPop = document.getElementById("user-pop-up");
+  const userBtn = document.getElementById("user-btn");
+
+  popBackground.addEventListener("click", () => {
+    userPop.classList.add("display-none");
+  });
+  userBtn.addEventListener("click", () => {
+    userPop.classList.remove("display-none");
+  });
+
+  const logOut = document.getElementById("log-out");
+
+  logOut.addEventListener("click", () => {
+    window.location.replace("/frontend/index.html");
+  });
 });
