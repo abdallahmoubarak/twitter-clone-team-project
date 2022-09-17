0d9eb838a99c4d05ac79659ca5cd2cb429662ade
Peter Saba <petersaba001@gmail.com>
2022-09-17 14:33:14 +0300

    implemented unfollow api using unfollow function

---
+++ backend/api/unfollow.php
@@ -0,0 +1,8 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+    include_once('unfollow_func.php');
+
+    $response['success'] = unfollow($_GET['follower_id'], $_GET['followed_id']);
+    echo json_encode($response);
+?>
