2ec5d51c2ba888690bdf65c6435a098ba7ce1c3a
Peter Saba <petersaba001@gmail.com>
2022-09-17 20:30:48 +0300

    added option to tweet without an image

--- backend/api/tweet.php
+++ backend/api/tweet.php
@@ -6,14 +6,22 @@
 
     $tweeter = $_POST['tweeter_id'];
     $content = $_POST['content'];
-    $image = $_POST['image'];
-    // creating a unique name for each tweet image
-    $image_url = 'id_' . $tweeter . '_' . date("Y-m-d_H-i-s") . '.jpg';
-    $image_url = convertToImg($image, $image_url);
-    $image_new_url = 'user_' . $tweeter . '/' . $image;
-    rename($image_url, $image_new_url);
-
-    $query = $mysqli->prepare('INSERT INTO tweets(tweeter_id, content, image_url) VALUES(?,?,?)');
-    $query->bind_param('iss', $tweeter, $content, $image_new_url);
-    $query->execute();
+    if(isset($_POST['image'])){
+        $image = $_POST['image'];
+        // creating a unique name for each tweet image
+        $image_url = 'id_' . $tweeter . '_' . date("Y-m-d_H-i-s") . '.jpg';
+        $image_url = convertToImg($image, $image_url);
+        $image_new_url = 'user_' . $tweeter . '/' . $image;
+        rename($image_url, $image_new_url);
+        
+        $query = $mysqli->prepare('INSERT INTO tweets(tweeter_id, content, image_url) VALUES(?,?,?)');
+        $query->bind_param('iss', $tweeter, $content, $image_new_url);
+        $query->execute();
+    }else{
+        $query = $mysqli->prepare('INSERT INTO tweets(tweeter_id, content) VALUES(?, ?)');
+        $query->bind_param('is', $tweeter, $content);
+        $query->execute();
+    }
+    $response['success'] = TRUE;
+    echo json_encode($response);
 ?>
