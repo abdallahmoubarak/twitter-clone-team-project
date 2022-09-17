9f364611097f8af6cab18fee5fe50a4c1c60a560
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-17 15:44:43 +0300

    ignore test

--- backend/api/connection.php
+++ backend/api/connection.php
@@ -3,7 +3,7 @@
     $host = "localhost";
     $username = "root";
     $password = null;
-    $db_name = "twitterdb";
+    $db_name = "twitter_db";
 
     $mysqli = new mysqli($host, $username, $password, $db_name);
 
