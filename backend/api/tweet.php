5e84b074992665889b750c4b8d86d0a2d2d49f2b
Peter Saba <petersaba001@gmail.com>
2022-09-17 20:21:05 +0300

    implemented tweet api

---
+++ backend/api/tweet.php
@@ -0,0 +1,19 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+    include_once('connection.php');
+    include_once('convert_images.php');
+
+    $tweeter = $_POST['tweeter_id'];
+    $content = $_POST['content'];
+    $image = $_POST['image'];
+    // creating a unique name for each tweet image
+    $image_url = 'id_' . $tweeter . '_' . date("Y-m-d_H-i-s") . '.jpg';
+    $image_url = convertToImg($image, $image_url);
+    $image_new_url = 'user_' . $tweeter . '/' . $image;
+    rename($image_url, $image_new_url);
+
+    $query = $mysqli->prepare('INSERT INTO tweets(tweeter_id, content, image_url) VALUES(?,?,?)');
+    $query->bind_param('iss', $tweeter, $content, $image_new_url);
+    $query->execute();
+?>
