e52fb3e3c16a448de30304a116b9a4914553f919
Peter Saba <petersaba001@gmail.com>
2022-09-18 05:06:30 +0300

    implemented api to get array of all tweets liked by the user

---
+++ backend/api/liked_tweets.php
@@ -0,0 +1,29 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+    include_once('connection.php');
+    include_once('convert_images.php');
+
+    $profile = $_GET['profile_id'];
+
+    // retrieving tweets liked by the user 
+    $query = $mysqli->prepare('SELECT t.id, t.tweeter_id, t.content, t.picture_url, t.created_at, t.likes_number, u.username, u.full_name
+                                FROM tweets AS t, users AS u, likes AS l 
+                                WHERE l.liker_id=? AND l.tweet_id = t.id AND t.tweeter_id = u.id
+                                ORDER BY t.created_at DESC');
+    $query->bind_param('i', $profile);
+    $query->execute();
+    $result = $query->get_result();
+
+    $response = [];
+    while($value = $result->fetch_assoc()){
+        // adding the base 64 image and removing the image path
+        if($value['picture_url']){
+            $value['image'] = convertToBase64($value['picture_url']);
+        }
+        unset($value['picture_url']);
+        $response[] = $value;
+    }
+
+    echo json_encode($response);
+?>
