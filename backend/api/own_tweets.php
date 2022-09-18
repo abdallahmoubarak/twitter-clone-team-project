814897e6fbed5196e2a311b4d91c09516610153b
Peter Saba <petersaba001@gmail.com>
2022-09-18 20:29:22 +0300

    sending image using url instead of bas64

--- backend/api/own_tweets.php
+++ backend/api/own_tweets.php
@@ -5,7 +5,7 @@
     $profile = $_GET['profile_id'];
 
     // retrieve array of tweets created by the user ordered from newest to oldest and his username and full name
-    $query = $mysqli->prepare('SELECT t.id, t.content, t.picture_url, t.likes_number, u.username, u.full_name
+    $query = $mysqli->prepare('SELECT t.id, t.content, t.picture_url, t.likes_number, t.created_at, u.username, u.full_name
                                 FROM tweets AS t, users AS U
                                 WHERE t.tweeter_id=? AND t.tweeter_id = u.id
                                 ORDER BY t.created_at DESC');
@@ -16,10 +16,10 @@
     $response = [];
     while($value = $result->fetch_assoc()){
         // removing the image url and adding the image in base 64 for each row retrieved
-        if($value['picture_url']){
-            $value['image'] = convertToBase64($value['picture_url']);
-        }
-        unset($value['picture_url']);
+        // if($value['picture_url']){
+        //     $value['image'] = convertToBase64($value['picture_url']);
+        // }
+        // unset($value['picture_url']);
         $response[] = $value;
     }
 
