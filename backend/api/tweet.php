46fbc44f63f89ec9edf0cc9a1d788e17b8f1b828
Peter Saba <petersaba001@gmail.com>
2022-09-18 13:43:25 +0300

    bug fix

--- backend/api/tweet.php
+++ backend/api/tweet.php
@@ -11,10 +11,10 @@
         // creating a unique name for each tweet image
         $image_url = 'id_' . $tweeter . '_' . date("Y-m-d_H-i-s") . '.jpg';
         $image_url = convertToImg($image, $image_url);
-        $image_new_url = 'user_' . $tweeter . '/' . $image;
+        $image_new_url = 'user_' . $tweeter . '/' . $image_url;
         rename($image_url, $image_new_url);
         
-        $query = $mysqli->prepare('INSERT INTO tweets(tweeter_id, content, image_url) VALUES(?,?,?)');
+        $query = $mysqli->prepare('INSERT INTO tweets(tweeter_id, content, picture_url) VALUES(?,?,?)');
         $query->bind_param('iss', $tweeter, $content, $image_new_url);
         $query->execute();
     }else{
